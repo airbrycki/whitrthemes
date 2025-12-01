c4eca05a75a6fea63111c1794b4affaf735a391b
airbrycki <whitney_airgood-obrycki@harvard.edu>
2025-12-01 16:07:05 -0500

    changing text sizes

--- R/theme_print.R
+++ R/theme_print.R
@@ -11,71 +11,90 @@
 #' @param base_line_size,base_rect_size base line and rectangle sizes
 #' @export
 
-theme_print <- function(base_size = 8.5, base_family = "Arial",
-                             base_line_size = 0.5,
-                             base_rect_size = 0.5) {
-
+theme_print <- function(
+  base_size = 8.5,
+  base_family = "Arial",
+  base_line_size = 0.5,
+  base_rect_size = 0.5
+) {
   half_line <- base_size / 2L
 
   ggplot2::theme(
-
     # main attributes
 
-    line = ggplot2::element_line(colour = "black",
-                                 size = base_line_size,
-                                 linetype = 1L,
-                                 lineend = "butt"),
-    rect = ggplot2::element_rect(fill = "white",
-                                 colour = "black",
-                                 size = base_rect_size,
-                                 linetype = 1L),
-    text = ggplot2::element_text(family = base_family,
-                                 face = "plain",
-                                 colour = "black",
-                                 size = base_size,
-                                 hjust = 0.5,
-                                 vjust = 0.5,
-                                 angle = 0,
-                                 lineheight = 0.9,
-                                 margin = ggplot2::margin(),
-                                 debug = FALSE),
+    line = ggplot2::element_line(
+      colour = "black",
+      size = base_line_size,
+      linetype = 1L,
+      lineend = "butt"
+    ),
+    rect = ggplot2::element_rect(
+      fill = "white",
+      colour = "black",
+      size = base_rect_size,
+      linetype = 1L
+    ),
+    text = ggplot2::element_text(
+      family = base_family,
+      face = "plain",
+      colour = "black",
+      size = 16,
+      hjust = 0.5,
+      vjust = 0.5,
+      angle = 0,
+      lineheight = 0.9,
+      margin = ggplot2::margin(),
+      debug = FALSE
+    ),
 
     # Plot Attributes
 
-    plot.tag = ggplot2::element_text(size = base_size * 1.5,
-                                     hjust = 0L,
-                                     vjust = 0L,
-                                     face = "bold",
-                                     margin = ggplot2::margin(b = 10L)),
+    plot.tag = ggplot2::element_text(
+      size = base_size * 1.5,
+      hjust = 0L,
+      vjust = 0L,
+      face = "bold",
+      margin = ggplot2::margin(b = 10L)
+    ),
     plot.tag.position = "topleft",
-    plot.title = ggplot2::element_text(size = base_size * 12 / 8.5,
-                                       hjust = 0L,
-                                       vjust = 0L,
-                                       face = "bold",
-                                       margin = ggplot2::margin(b = 10L)),
+    plot.title = ggplot2::element_text(
+      size = base_size * 20 / 8.5,
+      hjust = 0L,
+      vjust = 0L,
+      face = "bold",
+      margin = ggplot2::margin(b = 10L)
+    ),
     plot.title.position = "plot",
-    plot.subtitle = ggplot2::element_text(size = base_size * 9.5 / 8.5,
-                                          hjust = 0L,
-                                          vjust = 0L,
-                                          margin = ggplot2::margin(b = 10L)),
-    plot.caption = ggplot2::element_text(size = base_size * 7 / 8.5,
-                                         hjust = 1L,
-                                         vjust = 1L,
-                                         margin = ggplot2::margin(t = half_line * 0.9)),
+    plot.subtitle = ggplot2::element_text(
+      size = 18,
+      hjust = 0,
+      face = "bold",
+      margin = ggplot2::margin(b = 10)
+    ),
+    plot.caption = ggtext::element_textbox(
+      size = 17,
+      colour = "gray10",
+      width = unit(.999, "npc"),
+      hjust = -.4,
+      halign = 0,
+      lineheight = 1.3
+    ),
     plot.caption.position = "plot",
     plot.background = NULL,
 
-    plot.margin = ggplot2::margin(t = half_line,
-                                  r = base_line_size * 24,
-                                  b = half_line,
-                                  l = half_line),
+    plot.margin = ggplot2::margin(20, 40, 20, 20),
 
     # axis attributes
 
-    axis.text = ggplot2::element_text(size = base_size * 1.3),
-    axis.text.x = ggplot2::element_text(vjust = 1,
-                                        margin = ggplot2::margin(t = 4L),
-                                        size = base_size * 1.3),
+    axis.text = ggplot2::element_text(
+      size = 16,
+      margin = ggplot2::margin(r = 10)
+    ),
+    axis.text.x = ggplot2::element_text(
+      vjust = 1,
+      margin = ggplot2::margin(t = 4L),
+      size = 16
+    ),
     axis.text.y = ggplot2::element_text(hjust = 1),
     axis.text.x.top = NULL,
     axis.text.y.right = NULL,
@@ -86,10 +105,15 @@ theme_print <- function(base_size = 8.5, base_family = "Arial",
     axis.ticks.length.y.left = NULL,
     axis.ticks.length.y.right = NULL,
 
-    axis.title = ggplot2::element_text(size = base_size * 1.3, face = "bold"),
+    axis.title = ggplot2::element_text(
+      size = base_size * 18 / base_size,
+      face = "bold"
+    ),
     axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 8L)),
-    axis.title.y = ggplot2::element_text(angle = 90L,
-                                         margin = ggplot2::margin(r = 4L)),
+    axis.title.y = ggplot2::element_text(
+      angle = 90L,
+      margin = ggplot2::margin(r = 4L)
+    ),
     axis.title.x.top = NULL,
     axis.title.y.right = NULL,
 
@@ -97,12 +121,16 @@ theme_print <- function(base_size = 8.5, base_family = "Arial",
     axis.ticks.y = ggplot2::element_line(colour = "#BDBDBD"),
 
     axis.line = ggplot2::element_line(),
-    axis.line.x = ggplot2::element_line(size = 0.5,
-                                        linetype = "solid",
-                                        colour = "#BDBDBD"),
-    axis.line.y = ggplot2::element_line(size = 0.5,
-                                        linetype = "solid",
-                                        colour = "#BDBDBD"),
+    axis.line.x = ggplot2::element_line(
+      size = 0.5,
+      linetype = "solid",
+      colour = "#BDBDBD"
+    ),
+    axis.line.y = ggplot2::element_line(
+      size = 0.5,
+      linetype = "solid",
+      colour = "#BDBDBD"
+    ),
 
     # legend attributes
 
@@ -117,8 +145,7 @@ theme_print <- function(base_size = 8.5, base_family = "Arial",
     legend.key.height = NULL,
     legend.key.width = NULL,
 
-    legend.text = ggplot2::element_text(size = base_size * 1.5,
-                                        vjust = 0.5),
+    legend.text = ggplot2::element_text(size = base_size * 1.5, vjust = 0.5),
     legend.text.align = NULL,
     legend.title = ggplot2::element_blank(),
     legend.title.align = NULL,
@@ -153,26 +180,33 @@ theme_print <- function(base_size = 8.5, base_family = "Arial",
 
     # strip attributes (Faceting)
 
-    strip.background = ggplot2::element_rect(fill = "#dedddd",
-                                             colour = NA,
-                                             size = 10L),
-    strip.text = ggplot2::element_text(face = "bold",
-                                       size = base_size * 9.5 / 8.5,
-                                       margin = ggplot2::margin(t = 0L, r = 0L, b = 0L, l = 0L)),
+    strip.background = ggplot2::element_rect(
+      fill = "#dedddd",
+      colour = NA,
+      size = 10L
+    ),
+    strip.text = ggplot2::element_text(
+      face = "bold",
+      size = base_size * 9.5 / 8.5,
+      margin = ggplot2::margin(t = 0L, r = 0L, b = 0L, l = 0L)
+    ),
 
-    strip.text.x = ggplot2::element_text(margin = ggplot2::margin(t = 4.5, b = 4.5)),
-    strip.text.y = ggplot2::element_text(angle = -90L,
-                                         margin = ggplot2::margin(l = 4.5, r = 4.5)),
+    strip.text.x = ggplot2::element_text(
+      margin = ggplot2::margin(t = 4.5, b = 4.5)
+    ),
+    strip.text.y = ggplot2::element_text(
+      angle = -90L,
+      margin = ggplot2::margin(l = 4.5, r = 4.5)
+    ),
 
     strip.placement = "inside",
-    strip.placement.x =  NULL,
-    strip.placement.y =  NULL,
+    strip.placement.x = NULL,
+    strip.placement.y = NULL,
 
     strip.switch.pad.grid = ggplot2::unit(0.1, "cm"),
     strip.switch.pad.wrap = ggplot2::unit(0.1, "cm"),
 
     # create a complete format
     complete = TRUE
-
   )
 }
